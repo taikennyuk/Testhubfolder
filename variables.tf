8e1c7f3c75454aa10ce746e8fffe1b529a66d341
taikennyuk <cloud@3e9b17ecafd5>
2021-05-20 12:05:30 +0000

    first commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "subscriptionID" {
+    type = string
+    description = "4cedc5dd-e3ad-468d-bf66-32e31bdb9148"
+}
+
+variable "resourceGroupName" {
+    type = string
+    description = "1-c1e27c54-playground-sandbox"
+}
+
+variable "location" {
+    type = string
+    description = "westus"
+}
