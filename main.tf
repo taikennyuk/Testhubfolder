10415df0c08eab9f31a8ea152ac35a17fcab4c3d
taikennyuk <cloud@3e9b17ecafd5>
2021-05-20 11:48:34 +0000

    first commit

--- main.tf
+++ main.tf
@@ -1,27 +1,43 @@
 provider "azurerm" {
   version         = 2.0
-  subscription_id = var.subscriptionID
-      features {}
+    features {}
+
+}
+#resource "azurerm_resource_group" "example" {
+ # name     = "example-resources"
+  #location = "West Europe"
+#}
+
+resource "azurerm_virtual_network" "vnetexample" {
+  name                = "testvnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resourceGroupName
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resourceGroupName
-  location = var.location
+resource "azurerm_subnet" "subexample" {
+  name                 = "AzureFirewallSubnet"
+   resource_group_name = var.resourceGroupName
+  virtual_network_name = azurerm_virtual_network.vnetexample.name
+  address_prefix     = "10.0.1.0/24"
 }
 
-resource "azurerm_public_ip" "example1" {
-  name                = "PublicIPForLB"
+resource "azurerm_public_ip" "azurepip" {
+  name                = "testpip"
   location            = var.location
   resource_group_name = var.resourceGroupName
   allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
-resource "azurerm_lb" "example2" {
-  name                = "TestLoadBalancer"
-  location            = "West US"
+resource "azurerm_firewall" "example" {
+  name                = "testfirewall"
+  location            = var.location
   resource_group_name = var.resourceGroupName
-    frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.example1.id
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.subexample.id
+    public_ip_address_id = azurerm_public_ip.azurepip.id
   }
 }
